e194b1b7cda118a231cb0646812b704059876630
Mikhael Candra <tjandraraharja99@gmail.com>
2025-12-31 11:09:01 +0700

    implement route

--- src/app.controlle.ts
+++ src/app.controlle.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
 
-@Controller()
+@Controller() //jika ingin menambahkan prefix maka tambahkan di sini
 export class AppController {
   @Get()
   getRootRoute() {
     return 'Hello tod!';
+  }
+  @Get('/test')
+  getTestPage() {
+    return 'Hello ges!';
   }
 }
